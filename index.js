9e61f7e3bb0e042e6412f708a51e888547449e55
charles adah <charles.adah@centrifugegroup.com>
2023-12-13 02:27:15 -0800

    html and css assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+
+
+// let userName = prompt("Please enter your name:");
+// let verify = confirm('Would you like to subscribe for our news letter?');
+
+
+
+
+// let x = [35, 67, 70];
+
+// const getMostOccuringNumber = (sequence) => {
+//     if(resultMap.hasOwnProperty(number)) {
+        
+//     }
+// }
+
